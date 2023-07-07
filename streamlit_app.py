b7aba1695470c7e88472e22f6d65637f2918eb46
Danilo Oliveira <danilo.t.a.oliveira@gmail.com>
2023-07-07 14:01:47 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,22 @@
-from pyvis.network import Network
 import random
 import pandas as pd
+from pyvis import network as net
 import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
-got_net = Network(
+got_net = net.Network(
     notebook=True,
     cdn_resources="remote",
     bgcolor="#222222",
     font_color="white",
     height="750px",
     width="100%",
-    filter_menu=True
+    filter_menu=True,
 )
 
 got_net.barnes_hut()
-got_data = pd.read_csv("./Anime.csv")
+got_data = pd.read_csv("../Anime.csv")
 filtered_data = got_data.query('Release_year >= 2022.0')
 filtered_data = filtered_data.loc[:, ['Name', 'Voice_actors']]
 filtered_data = filtered_data.dropna(subset=['Voice_actors'])
@@ -24,6 +24,7 @@ filtered_data['Voice_actors'] = filtered_data['Voice_actors'].str.split('\n').st
 filtered_data = filtered_data[filtered_data['Voice_actors'].str.contains(':')]
 
 obras_atores = {}
+node_attributes = {}  # Dicionário auxiliar para armazenar os atributos dos nós
 
 unique_obras = filtered_data['Name'].unique()
 num_obras = len(unique_obras)
@@ -39,8 +40,9 @@ for index, row in filtered_data.iterrows():
     a = actor.split(':')
     if len(a) > 1:
       actor_name = a[1].strip()
-      if actor_name not in got_net.nodes:
-        got_net.add_node(actor_name, label=actor_name)
+      if actor_name not in node_attributes:
+        got_net.add_node(actor_name)
+        node_attributes[actor_name] = {'name': actor_name}  # Armazenar atributo name do nó
 
       if name in obras_atores:
         obras_atores[name].append(actor_name)
@@ -57,6 +59,20 @@ for obra, atores in obras_atores.items():
         color = obras_colors[obra]  # Obtém a cor aleatória correspondente à obra
         got_net.add_edge(ator1, ator2, title=obra, label=obra, color=color)  # Adicionar rótulo e cor para cada aresta
 
+
+# Obter a lista de nós
+node_list = sorted(node_attributes.keys())
+
+# Menu suspenso para seleção do nó
+selected_node = st.sidebar.selectbox('Selecione um nó', node_list)
+
+# Exibir o nome do nó selecionado
+st.write("Nome do nó selecionado:", selected_node)
+
+# Marcar o nó selecionado no gráfico
+for node_name in got_net.nodes:
+  if node_name["id"] == selected_node:
+    node_name['color'] = 'red'  # Definir a cor do nó destacado como 'red'
 
 # Exibir o gráfico de rede
 got_net.show("animes.html")
